9d3c6f3d60e7caaf219663355d68dcb1ba1d2f92
Sam Rang <srang2010@gmail.com>
2023-10-25 13:50:11 -0400

    24. Swap Nodes in Pairs

---
+++ src/com/github/srang/leetcode/swap_node_pair/Solution.java
@@ -0,0 +1,21 @@
+package com.github.srang.leetcode.swap_node_pair;
+
+import com.github.srang.leetcode.helper.ListNode;
+
+public class Solution {
+    public static ListNode swapPairs(ListNode head) {
+        ListNode tmp = head;
+        if (tmp != null && tmp.next != null) {
+            ListNode swap1 = head;
+            ListNode swap2 = head.next;
+            head = swap2;
+            swap1.next = swapPairs(swap2.next);
+            swap2.next = swap1;
+        } else {
+            return head;
+        }
+
+
+        return head;
+    }
+}
---
+++ src/com/github/srang/leetcode/swap_node_pair/SolutionTest.java
@@ -0,0 +1,49 @@
+package com.github.srang.leetcode.swap_node_pair;
+
+import com.github.srang.leetcode.helper.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+    @Test
+    void testNodePair1() {
+        ListNode test = new ListNode(new int[]{1,2,3,4});
+        ListNode answer = new ListNode(new int[]{2,1,4,3});
+        assertEquals(answer.toString(), Solution.swapPairs(test).toString());
+    }
+
+    @Test
+    void testNodePair2() {
+        ListNode test = new ListNode();
+        assertEquals(null, Solution.swapPairs(test));
+    }
+
+    @Test
+    void testNodePair3() {
+        ListNode test = new ListNode(new int[]{1});
+        ListNode answer = new ListNode(new int[]{1});
+        assertEquals(answer.toString(), Solution.swapPairs(test).toString());
+    }
+
+    @Test
+    void testNodePair4() {
+        ListNode test = new ListNode(new int[]{1,2,3,4,5});
+        ListNode answer = new ListNode(new int[]{2,1,4,3,5});
+        assertEquals(answer.toString(), Solution.swapPairs(test).toString());
+    }
+
+    @Test
+    void testNodePair5() {
+        ListNode test = new ListNode(new int[]{1,2,3,4,5,6,7,8,9,10});
+        ListNode answer = new ListNode(new int[]{2,1,4,3,6,5,8,7,10,9});
+        assertEquals(answer.toString(), Solution.swapPairs(test).toString());
+    }
+
+    @Test
+    void testNodePair6() {
+        ListNode test = new ListNode(new int[]{1,2});
+        ListNode answer = new ListNode(new int[]{2,1});
+        assertEquals(answer.toString(), Solution.swapPairs(test).toString());
+    }
+}
