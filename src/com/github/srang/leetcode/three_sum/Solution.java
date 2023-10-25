211281c18593bf85e5d13e30ffc37334ebefa8c0
Sam Rang <srang2010@gmail.com>
2023-10-25 13:00:12 -0400

    15. Three Sum (#4)

---
+++ src/com/github/srang/leetcode/three_sum/Solution.java
@@ -0,0 +1,29 @@
+package com.github.srang.leetcode.three_sum;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Solution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> tempSolution = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    tempSolution.add(Stream.of(nums[i], nums[j], nums[k]).sorted().toList());
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return new ArrayList<>(tempSolution);
+    }
+}
